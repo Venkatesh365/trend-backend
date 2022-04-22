b420bab44f1aba147ba17381326666347131998b
Venkatesh365 <evv.pedagadi365@gmail.com>
2022-04-22 11:24:07 +0530

    make it better

--- index.js
+++ index.js
@@ -94,6 +94,12 @@ app.post("/users/", async (request, response) => {
    }
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+
  //user login//
  app.post("/login", async (request, response) => {
   const { username, password } = request.body;
