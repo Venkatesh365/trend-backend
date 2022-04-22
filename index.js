cfac878363c121594746a6e9769f5a975cc6b31b
Venkatesh365 <evv.pedagadi365@gmail.com>
2022-04-22 15:49:44 +0530

    make it better

--- index.js
+++ index.js
@@ -96,7 +96,6 @@ app.post("/users/", async (request, response) => {
 
   app.use(function(req, res, next){
     res.removeHeader('Access-Control-Allow-Origin');
-    res.header('Access-Control-Allow-Origin', 'https://*.mycompany.com'); 
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
