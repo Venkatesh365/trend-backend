3e9563a572667bae681ee85bd0aaaa4c44c6ef4f
Venkatesh365 <evv.pedagadi365@gmail.com>
2022-04-22 13:15:37 +0530

    make it better

--- index.js
+++ index.js
@@ -94,11 +94,12 @@ app.post("/users/", async (request, response) => {
    }
   });
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
-
+ });
 
  //user login//
  app.post("/login", async (request, response) => {
